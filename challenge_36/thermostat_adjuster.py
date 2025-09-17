6ec540c30b9a413c20b4f1452094f6ba31703f9f
KavinduDr <rmkavindudhananjaya@gmail.com>
2025-09-17 06:30:01 +0530

    Fix print statements in thermostat adjuster for clarity

--- challenge_36/thermostat_adjuster.py
+++ challenge_36/thermostat_adjuster.py
@@ -6,6 +6,6 @@ def adjust_thermostat(current_temp, target_temp):
     else:
         return "hold"
 
-print("\n" + adjust_thermostat(65, 70))  # Output: "heat"
-print("\n" + adjust_thermostat(75, 70))  # Output: "cool"
-print("\n" + adjust_thermostat(70, 70))  # Output: "hold
+print("\n" + adjust_thermostat(65, 70))
+print("\n" + adjust_thermostat(75, 70))
+print("\n" + adjust_thermostat(70, 70))
